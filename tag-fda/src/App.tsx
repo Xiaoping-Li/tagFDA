b9fb15567b5430297305de9a999c0daeb95eedbc
Xiaoping-Li <narci2008@gmail.com>
2019-08-28 15:44:28 -0700

    Init project

---
+++ tag-fda/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      WELCOME TO TAG FDA
+    </div>
+  );
+}
+
+export default App;

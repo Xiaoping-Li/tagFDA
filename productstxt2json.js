f7e7eb3eac7e083e9eebd6f629f1805940ee7d1a
Xiaoping-Li <narci2008@gmail.com>
2019-08-29 15:43:36 -0700

    Add script to convert products.txt to json file

---
+++ productstxt2json.js
@@ -0,0 +1,43 @@
+const drug_file = './drugsatfda20190827/Products.txt';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const lineReader = readline.createInterface({
+  input: fs.createReadStream(drug_file)
+});
+
+let isHeader = false;
+let colNames = [];
+
+function parseLine(line) {
+  return line.trim().split('\t');
+}
+
+function createRowObject(values) {
+  const rowobject = {};
+  colNames.forEach((val, idx) => {
+    if (val !== 'ReferenceDrug' && val !== 'ReferenceStandard') {
+      rowobject[val] = values[idx];
+    }
+  });
+  return rowobject;
+}
+
+const json = [];
+
+lineReader.on('line', function(line) {
+  if(!isHeader) {
+    colNames = parseLine(line);
+    isHeader = true;
+  } else {
+    json.push(createRowObject(parseLine(line)));
+  }
+});
+
+lineReader.on('close', function() {
+  fs.writeFileSync(drug_file + '.json', JSON.stringify(json, null, 2));
+});
+
+
+
